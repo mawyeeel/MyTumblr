bba48c6f36d32a3f8bed4fda8ae7c33528d06ad6
LimboMarielleAngelCharishe <mawyeeel.school@gmail.com>
2022-05-18 04:00:22 +0800

    php login script added

--- account.php
+++ account.php
@@ -1,3 +1,19 @@
+<?php
+//start the session 
+session_start();
+
+//check if the session variables are set or meron lamana ang session variable or naga exist sila 
+//this is to check if naka login pa yung account or hindi na 
+if (isset($_SESSION['ses_username'])==false){
+    header("Location: index.php?logfirst");
+}elseif (isset($_REQUEST['logout'])===true){
+    session_destroy();
+    header("Location: index.php?logout");
+}
+
+
+?>
+
 <!doctype html>
                         <html>
                             <head>
@@ -13,14 +29,13 @@
     transform: translateY(5rem)
 }
 .cover {
-    background-image: url('images/photo-profile-background.avif');
+    background-image: url('images/profile-background.avif');
     background-size: cover;
     background-repeat: no-repeat
 }
 
 body {
-    background: #654ea3;
-    background: linear-gradient(to right, #e96443, #904e95);
+    background-image: url('images/body-background.avif');
     min-height: 100vh;
     overflow-x: hidden
 }</style>
@@ -32,27 +47,30 @@ body {
         <div class="bg-white shadow rounded overflow-hidden">
             <div class="px-4 pt-0 pb-4 cover">
                 <div class="media align-items-end profile-head">
-                    <div class="profile mr-3"><img src="images/photo-profile.avif" alt="..." width="150" class="rounded mb-2 img-thumbnail"><a href="#" class="btn btn-outline-dark btn-sm btn-block">Edit Profile</a>
+                    <div class="profile mr-3"><img src="images/profile.avif" alt="..." width="150" class="rounded mb-2 img-thumbnail">
+                        <a href="?logout" class="btn btn-outline-dark btn-sm btn-block">Log Out </a>
 
 
                     </div>
                     <div class="media-body mb-5 text-white">
-                        <h4 class="mt-0 mb-0">Ryan Clifford L. Perez</h4>
-                        <p class="small mb-4"> <i class="fas fa-map-marker-alt mr-2"></i>Marinduque PH
+                        <h4 class="mt-0 mb-0"> <?php echo $_SESSION['ses_fullname']; ?> </h4>
+                        <p class="small mb-4"> <i class="fas fa-map-marker-alt mr-2"></i>
+
+                            <?php echo $_SESSION['ses_adress']; ?>
                          </p>
                     </div>
                 </div>
             </div>
-            <div class="bg-light p-4 d-flex justify-content-end text-center">
+            <div class="bg-black p-4 d-flex justify-content-end text-center">
                 <ul class="list-inline mb-0">
                     <li class="list-inline-item">
-                        <h5 class="font-weight-bold mb-0 d-block">21,150</h5><small class="text-muted"> <i class="fas fa-image mr-1"></i>Photos</small>
+                        <h5 class="font-weight-bold mb-0 d-block">14,890</h5><small class="text-muted"> <i class="fas fa-image mr-1"></i>Photos</small>
                     </li>
                     <li class="list-inline-item">
-                        <h5 class="font-weight-bold mb-0 d-block">756,450</h5><small class="text-muted"> <i class="fas fa-user mr-1"></i>Followers</small>
+                        <h5 class="font-weight-bold mb-0 d-block">53,285</h5><small class="text-muted"> <i class="fas fa-user mr-1"></i>Followers</small>
                     </li>
                     <li class="list-inline-item">
-                        <h5 class="font-weight-bold mb-0 d-block">340</h5><small class="text-muted"> <i class="fas fa-user mr-1"></i>Following</small>
+                        <h5 class="font-weight-bold mb-0 d-block">264</h5><small class="text-muted"> <i class="fas fa-user mr-1"></i>Following</small>
                     </li>
                 </ul>
             </div>
@@ -60,9 +78,9 @@ body {
             <div class="px-4 py-3">
                 <h5 class="mb-0">Expertise </h5>
                 <div class="p-4 rounded shadow-sm bg-light">
-                    <p class="font-italic mb-0">Full-stack Web Developer</p>
+                    <p class="font-italic mb-0">Gaming</p>
                     <p class="font-italic mb-0">Mobile App Developer</p>
-                    <p class="font-italic mb-0">Photographer/Videographer</p>
+                    <p class="font-italic mb-0">Photographer</p>
                 </div>
             </div>
 
@@ -72,10 +90,10 @@ body {
                 </div>
                 <div class="row">
                     
-                    <div class="col-lg-6 mb-2 pr-lg-1"><img src="images/photo-1.avif" alt="" class="img-fluid rounded shadow-sm"></div>              
-                    <div class="col-lg-6 mb-2 pl-lg-1"><img src="images/photo-2.avif" alt="" class="img-fluid rounded shadow-sm"></div>                
-                    <div class="col-lg-6 pr-lg-1 mb-2"><img src="images/photo-3.avif" alt="" class="img-fluid rounded shadow-sm"></div>
-                    <div class="col-lg-6 pl-lg-1"><img src="images/photo-4.avif" alt="" class="img-fluid rounded shadow-sm"></div>
+                    <div class="col-lg-6 mb-2 pr-lg-1"><img src="images/pic-1.avif" alt="" class="img-fluid rounded shadow-sm"></div>              
+                    <div class="col-lg-6 mb-2 pl-lg-1"><img src="images/pic-2.avif" alt="" class="img-fluid rounded shadow-sm"></div>                
+                    <div class="col-lg-6 pr-lg-1 mb-2"><img src="images/pic-3.avif" alt="" class="img-fluid rounded shadow-sm"></div>
+                    <div class="col-lg-6 pl-lg-1"><img src="images/pic-4.avif" alt="" class="img-fluid rounded shadow-sm"></div>
                 </div> 
             </div>
 
--- index.php
+++ index.php
@@ -1,3 +1,44 @@
+<?php
+//start the session function
+session_start();
+
+//create a pre defined username and password since we do not have database
+$acc_username = "MarielleLimbo";
+$acc_password = "marielle54321";
+$acc_fullname = "Marielle Angel Charishe Limbo";
+$acc_adress = "Boac, Marinduque  PH";
+//check the current url for the redirections later
+$url_add = "http://".$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
+
+
+//condition to know if the button is clicked
+if(isset($_REQUEST['login_button']) === true){
+  //get the username and password from the form and compare to the predefined username and password
+  //kapag mali agad yung username
+   if($_REQUEST['form_username'] != $acc_username){
+       header("Location: ".$url_add."?notexist");
+   }
+   //tama ang username pero mali ang password
+   else if ($_REQUEST['form_username'] ==$acc_username  && $_REQUEST['form_password'] != $acc_password){
+     header("Location: ".$url_add."?wrongpass");
+   }
+   //tama ang username at password 
+   else if ($_REQUEST['form_username']==$acc_username && $_REQUEST['form_password'] == $acc_password){
+     header("Location: ".$url_add."?success");
+
+     //create a session variables
+     $_SESSION['ses_username'] = $acc_username;
+     $_SESSION['ses_password'] = $acc_password;
+     $_SESSION['ses_fullname'] = $acc_fullname;
+     $_SESSION['ses_adress'] = $acc_adress;
+
+   }//end of correct username and password 
+
+
+} //end of loginbutton
+
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -23,13 +64,47 @@
 		      		<span class="fa fa-user-o"></span>
 		      	</div>
 		      	<h3 class="text-center mb-4">MyTumblr Login</h3>
+
+
+
 						
-						<form action="#" class="login-form">
+						<form method="POST" class="login-form">
+
 		      		<div class="form-group">
-		      			<input type="text" class="form-control rounded-left" placeholder="Username" required>
+
+		      			<?php
+		      			//this is the messaging
+                 if(isset($_REQUEST['notexist'])===true){
+                       echo "<div class='alert alert-danger' role='alert'> Username does not exist... </div>";
+
+                 } else if (isset($_REQUEST['wrongpass'])===true){
+                        echo "<div class='alert alert-warning' role='alert'> Incorrect Password... </div>";
+
+                 } else if (isset($_REQUEST['success'])===true){
+                        echo "<div class='alert alert-success' role='alert'> Redirecting... </div>";
+                        header ("Refresh: 5; url=account.php");
+
+
+                 }else if (isset($_REQUEST['logout'])===true){
+
+                        echo "<div class='alert alert-info' role='alert'> Thank You... </div>";
+
+                 }else if (isset($_REQUEST['logfirst'])===true){
+                 				echo "<div class='alert alert-info' role='alert'> Please Login First. </div>";
+
+                 }else if (isset($_SESSION['ses_username'])===true){
+                        echo "<div class='alert alert-warning' role='alert'> You are still logged in. Please <a href='account.php'> click here </a> to proceed. </div>";
+
+                 }
+
+                 ?>
+                
+
+
+		      			<input type="text" class="form-control rounded-left" placeholder="Username" name="form_username" required>
 		      		</div>
 	            <div class="form-group d-flex">
-	              <input type="password" class="form-control rounded-left" placeholder="Password" required>
+	              <input type="password" class="form-control rounded-left" placeholder="Password" name="form_password" required>
 	            </div>
 	            <div class="form-group d-md-flex">
 	            	<div class="w-50">
@@ -43,9 +118,15 @@
 								</div>
 	            </div>
 	            <div class="form-group">
-	            	<button type="submit" class="btn btn-primary rounded submit p-3 px-5">Get Started</button>
+
+	            	<button type="submit" class="btn btn-primary rounded submit p-3 px-5" name="login_button">Get Started</button>
 	            </div>
 	          </form>
+
+
+
+
+
 	        </div>
 				</div>
 			</div>
